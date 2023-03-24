faf97f263b3295f98a74e28ef09d4c026e55247e
Tristverm <emmanuelkyalo72@gmail.com>
2023-03-24 22:49:58 +0300

    if statement

---
+++ conditional-statements.js
@@ -0,0 +1,19 @@
+// In JavaScript, an if statement is a control statement that allows you to execute a block of code conditionally. The if statement evaluates a specified condition, and if the condition is true, the block of code inside the if statement is executed. If the condition is false, the block of code inside the if statement is skipped.
+
+// Here's the basic syntax of an if statement in JavaScript:
+if (condition) {
+  // code to execute if condition is true
+}
+//   In this syntax, condition is an expression that is evaluated to either true or false. If the condition is true, the code inside the curly braces will be executed. If the condition is false, the code inside the curly braces will be skipped.
+
+// Here's an example:
+
+let age = 18;
+
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are not yet an adult.");
+}
+
+// In this example, the if statement checks whether age is greater than or equal to 18. Since age is 18, the condition is true, and the code inside the first set of curly braces is executed. The output of this code would be: "You are an adult."
