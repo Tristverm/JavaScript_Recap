43d04487711e669d74a20d8d41c80d91ce4fea48
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-03 08:21:47 +0300

    Added Creating Maps

---
+++ Maps/creating_maps.js
@@ -0,0 +1,13 @@
+//You can create a Map in JavaScript by using the Map() constructor function. Here's an example:
+// create a new Map
+let myMap = new Map();
+
+// add some key-value pairs to the map
+myMap.set('key1', 'value1');
+myMap.set('key2', 'value2');
+
+// retrieve values from the map using the get() method
+console.log(myMap.get('key1')); // output: 'value1'
+console.log(myMap.get('key2')); // output: 'value2'
+
+//In this example, we first create a new Map by calling the Map() constructor function without any arguments. Then we use the set() method to add two key-value pairs to the map. Finally, we retrieve the values associated with the keys 'key1' and 'key2' using the get() method and log them to the console.
