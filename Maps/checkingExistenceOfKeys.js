4476e372f2f2f37840932cec5e728bba78e64b58
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-03 09:34:56 +0300

    Added Chekcing the Existnece of Keys in  Maps

---
+++ Maps/checkingExistenceOfKeys.js
@@ -0,0 +1,10 @@
+// create a new Map
+let myMap = new Map();
+
+// add some key-value pairs to the map
+myMap.set('key1', 'value1');
+myMap.set('key2', 'value2');
+
+// use the has() method to check if the map contains a specific key
+console.log(myMap.has('key1')); // output: true
+console.log(myMap.has('key3')); // output: false
