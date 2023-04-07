7cd063b603ef534c1adbc380c21c42f46165661a
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-07 17:35:09 +0300

    Added brackets in Objects

---
+++ objects/squarebrackets.js
@@ -0,0 +1,35 @@
+// Here's an example of dynamically accessing and setting properties on a JavaScript object using bracket notation:
+const person = {
+    name: 'John',
+    age: 30,
+  };
+  
+  // Dynamically access property using bracket notation
+  const propertyName = 'name';
+  console.log(person[propertyName]); // Output: 'John'
+  
+  // Dynamically set property using bracket notation
+  const newPropertyName = 'address';
+  person[newPropertyName] = '123 Main St';
+  console.log(person.address); // Output: '123 Main St'
+  
+  // Define a non-enumerable property
+  Object.defineProperty(person, 'ssn', {
+    value: '123-45-6789',
+    writable: false,
+    enumerable: false,
+  });
+  console.log(person.ssn); // Output: '123-45-6789'
+  person.ssn = '987-65-4321'; // This will fail silently in strict mode
+  for (const key in person) {
+    console.log(key); // Output: 'name', 'age', 'address' (but not 'ssn')
+  }
+  
+
+  //In this example, we define an object person with two properties, name and age. We then use bracket notation to dynamically access the name property using a variable propertyName, and to dynamically set a new address property using a variable newPropertyName.
+
+
+  //Finally, we use Object.defineProperty() to define a new property ssn on the person object, but we set it to be non-enumerable and non-writable. This means that it cannot be accessed or modified using dot or bracket notation, and it will not be included in a for...in loop over the object's properties.
+
+
+
