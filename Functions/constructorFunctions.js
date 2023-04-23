8c7b9dcd94ac0d135355862a82a51e8d733d2b0d
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-23 18:31:51 +0300

    Added Constructor Functions

--- Functions/constructorFunctions.js
+++ Functions/constructorFunctions.js
@@ -35,5 +35,17 @@ console.log(john.fullName()); // "John Doe"
 //In summary, function constructors in JavaScript are a way to create objects based on a given template or blueprint. The constructor function defines the properties and methods of the object, and each instance created from the constructor will inherit those properties and methods.
 
 // remember that the caps Person is only syntatctical sugar 
+//In the example code provided, the capital "P" in "Person" is simply a convention for indicating that the function is intended to be used as a constructor. By convention, constructor functions in JavaScript are typically named with a capital letter at the beginning of the name, to distinguish them from regular functions.
+//The purpose of using a constructor function is to create new objects with a common structure and behavior, based on the definition of the constructor function. In this example, the Person constructor function creates objects that represent people, with properties such as firstName, lastName, and age, and a method called fullName.
+
+//When we use the "new" keyword to create a new object from the constructor function, JavaScript sets up the object's internal structure according to the definition of the constructor function. This allows us to create multiple instances of the same type of object, with consistent structure and behavior, without having to define each one manually.
+//So, the capital "P" in "Person" simply serves as a convention to indicate that this is a constructor function, and helps to distinguish it from other functions in the code.
+
+
+
+
+
+
+
 
 
