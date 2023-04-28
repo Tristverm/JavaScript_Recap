d32235f9a74504e428ec39f4cdc784c92b4b889d
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-28 23:01:07 +0300

    Added Purefunctions

--- Functions/pureFunction.js
+++ Functions/pureFunction.js
@@ -27,3 +27,31 @@ console.log(arr); // Output: [1, 2, 3, 4]
 
 //The slice() method returns a new array containing a portion of the original array, but it does not modify the original array. It takes two arguments, the start index and the end index of the portion of the array to be sliced, and returns a new array containing the elements in that range.
 
+
+//Impure Functions
+
+
+//An impure function is a function that does not always return the same output given the same input, and/or has side effects. In other words, it modifies something outside of its own scope, or depends on something outside of its own scope.
+
+
+//Here is an example of an impure function:
+
+let counter = 0;
+
+function increment() {
+  counter++;
+  return counter;
+}
+
+
+// The increment function is impure because it modifies the counter variable outside of its own scope. Every time it is called, it increases the value of counter by 1 and returns the new value.
+
+
+//Another example of an impure function is the Math.random() method:
+
+function getRandomNumber() {
+    return Math.random();
+  }
+
+  //The getRandomNumber function is impure because it does not always return the same output given the same input. It generates a random number between 0 and 1 every time it is called, so the output is unpredictable and not deterministic.
+
