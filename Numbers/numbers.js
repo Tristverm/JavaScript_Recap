f64123d9b9f0e28e569f276b7e42eb992de7146f
Tristverm <emmanuelkyalo72@gmail.com>
2023-04-29 23:56:25 +0300

    Added Factory Numbers

---
+++ Numbers/numbers.js
@@ -0,0 +1,3 @@
+// In javascript every number is a float, JS does not have special number data types as seen in other programming languages
+
+// NUmbers are stored as 64 bit floating points in JavaScript
